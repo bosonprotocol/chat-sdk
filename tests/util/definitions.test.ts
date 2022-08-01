7cb60d802e67c7d647e1c9124c1b6176a39f22c3
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2022-08-01 14:43:32 +0200

    feat: remove all tests related files (#2)

--- tests/util/definitions.test.ts
+++
@@ -1,9 +0,0 @@
-import {
-  MessageType,
-  SupportedImageMimeTypes
-} from "../../src/util/definitions";
-
-test("Import enum definitions", () => {
-  expect(MessageType).toBeTruthy();
-  expect(SupportedImageMimeTypes).toBeTruthy();
-});
